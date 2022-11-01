6e264afa3125a6837622df48989fee14fbc168b9
marek <marek@LENOVO-MAREK>
2022-11-01 13:41:09 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ServicesManager;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private TaskbarIcon tray;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        tray = (TaskbarIcon)FindResource("NotifyIcon");
+        tray.Icon = System.Drawing.SystemIcons.Exclamation;
+        tray.TrayRightMouseDown += BeforeShowContextMenuEvent;
+        SetMenu();
+    }
+    
+    protected override void OnExit(ExitEventArgs e)
+    {
+        tray.Dispose();
+        base.OnExit(e);
+    }
+
+    private List<string> GetServices()
+    {
+        string listOfServices = System.IO.File.ReadAllText(@"RequestedServices.txt");
+        return listOfServices.Split(';').ToList();
+    }
+
+    private void SetMenu()
+    {
+        tray.ContextMenu = new ContextMenu();
+        var requestedServices = GetServices();
+        var selectedServices = ServiceController.GetServices().Where(x => requestedServices.Contains(x.ServiceName)).ToList();
+
+        foreach (var selectedService in selectedServices)
+        {
+            var item = new MenuItem()
+            {
+                Header = $"{selectedService.ServiceName} | {selectedService.Status.ToString()}",
+                Background = selectedService.Status == ServiceControllerStatus.Running
+                    ? new SolidColorBrush(Colors.GreenYellow)
+                    : new SolidColorBrush(Colors.Red),
+
+            };
+            item.Command = StartStopService(selectedService);
+            tray.ContextMenu.Items.Add(item);
+        }
+    }
+
+    private ICommand StartStopService(ServiceController service)
+    {
+
+        return new DelegateCommand()
+        {
+            CommandAction = () =>
+            {
+                if (service.Status == ServiceControllerStatus.Stopped ||
+                    service.Status == ServiceControllerStatus.Running)
+                {
+                    if (service.Status == ServiceControllerStatus.Running) service.Stop();
+                    else service.Start();
+                    SetMenu();
+                }
+            }
+        };
+    }
+
+    private void BeforeShowContextMenuEvent(object sender, RoutedEventArgs e)
+    {
+        SetMenu();
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+﻿# Windows Service Manager
+Simple application, launched in tray to management selected windows services.
+
+## How to run?
+Before running the application, fill out *RequestedServices.txt*, enter the selected service names, separated by semicolons [ ; ]. You have to run the application as an administrator, because higher permissions are needed to connect to Windows services.
